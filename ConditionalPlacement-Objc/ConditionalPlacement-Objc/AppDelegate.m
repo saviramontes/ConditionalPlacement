0c53f4c3226b6a4e9b6b73e36e17b2d2fa289673
Stephen Viramontes <saviramontes@asu.edu>
2015-04-29 16:23:02 -0600

    Finsihed
    Done

--- ConditionalPlacement-Objc/ConditionalPlacement-Objc/AppDelegate.m
+++ ConditionalPlacement-Objc/ConditionalPlacement-Objc/AppDelegate.m
@@ -19,6 +19,35 @@
 
     // Insert code here...
     
+    int karl =10;
+    int leonard = 7;
+    
+    int karlPlace;
+    int leonardPlace;
+    
+    karlPlace = karl >7 ? 1:2;
+    leonardPlace = leonard >7 ? 1:2;
+    
+    if(karlPlace == 1 && leonardPlace ==1) {
+     
+        NSLog(@"Both equal to 1");
+    }
+    
+   else if(karlPlace == 1) {
+        
+        NSLog(@"karl is placed 1 and leonard is placed 2");
+    }
+   else if(leonardPlace ==1) {
+            
+        NSLog(@"leonard is placed 1 and karl is placed 2");
+    }
+        
+   else {
+            
+        NSLog(@"karl and leonard are placed 2");
+    
+   }
+    
     
     return YES;
 }
